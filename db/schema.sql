1414728e1c1c775d0586754a8a0b53e11ff787c6
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-17 18:37:28 -0500

    writing schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary FLOAT NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id)
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
+
+
