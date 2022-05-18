6459853f23d643718bee0d978a1d3e1d30cf4b12
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-17 19:14:47 -0500

    fixing schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -10,11 +10,11 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary FLOAT NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
+  department_id INT,
+--   PRIMARY KEY (id)
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -22,16 +22,15 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
-  PRIMARY KEY (id)
+  role_id INT,
+  manager_id INT,
+--   PRIMARY KEY (id)
   FOREIGN KEY (role_id)
   REFERENCES role(id)
 --   FOREIGN KEY (manager_id)
+--   REFERENCES employee(id)
   ON DELETE SET NULL
 );
-
-
