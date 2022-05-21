aa151c361705c7d207acb6c42111090a923136bd
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-21 09:03:17 -0500

    adding .env file

--- scripts/connection.js
+++ scripts/connection.js
@@ -1,10 +1,12 @@
 const db = mysql.createConnection(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    process.env.DB_HOST,
     {
-      host: 'localhost',
-      user: 'root',
-      password: 'rootroot',
-      database: 'employees_db'
-    },
+      dialect: 'mysql',
+      port: 3306
+    }
     console.log(`Connected to the employees_db database.`)
   );
 
